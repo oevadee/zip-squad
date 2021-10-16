395d0852e559dc6dc7e59fde1fd59959c4822365
Adrian Szczechura <a.szczechura.98@gmail.com>
2021-10-16 04:12:41 +0200

    feat(frontend): add neo shadows to components, logout button in sidebar, refactor button

--- frontend/src/components/input/index.tsx
+++ frontend/src/components/input/index.tsx
@@ -27,6 +27,7 @@ const SInput = styled.input`
     color: ${({ theme }) => theme.colors.white};
     outline: none;
     transition: ${({ theme }) => theme.transitions.ease};
+    box-shadow: rgba(0, 0, 0, 0.3) 0px 19px 38px, rgba(0, 0, 0, 0.22) 0px 15px 12px;
 
     &:focus {
         border: solid 1px ${({ theme }) => theme.colors.primary};
