0a81c3321541ccdb1a552e13e5c84d0908c99ad0
Adrian Szczechura <a.szczechura.98@gmail.com>
2021-10-17 15:10:16 +0200

    fix(frontend): fix and applied linter rules

--- frontend/svg.d.ts
+++ frontend/svg.d.ts
@@ -1,5 +1,5 @@
 // react-app-env.d.ts
 declare module '*.svg' {
-    import React, { FunctionComponent, SVGProps } from 'react';
+    import { FunctionComponent, SVGProps } from 'react';
     export const ReactComponent: FunctionComponent<SVGProps<SVGSVGElement> & { title?: string }>;
 }
